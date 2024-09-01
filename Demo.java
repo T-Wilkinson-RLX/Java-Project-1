a092a4788057ae1b1b23e7c152b04fb81d9f2c34
Tauriq-Allie Wilkinson <tauriqalliew@gmail.com>
2024-09-01 15:21:04 +0200

    IntelliJ

--- Demo.java
+++ Demo.java
@@ -1,16 +1,4 @@
 
-class Mobile{
-
-    String brand;
-    int cost;
-    static String name;
-
-    public void show(){
-
-        System.out.println(brand + " : " + cost + " : " + name);
-    }
-
-}
 
 
 public class Demo {
---
+++ Mobile.java
@@ -0,0 +1,12 @@
+ class Mobile {
+
+    String brand;
+    int cost;
+    static String name;
+
+    public void show(){
+
+        System.out.println(brand + " : " + cost + " : " + name);
+    }
+
+}
