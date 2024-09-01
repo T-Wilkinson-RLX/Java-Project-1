4e72266b8775b18e62115b8cf56f04167a930c26
Tauriq-Allie Wilkinson <tauriqalliew@gmail.com>
2024-09-01 14:04:13 +0200

    update

--- Demo.java
+++ Demo.java
@@ -21,12 +21,14 @@ public class Demo {
         Mobile obj1 = new Mobile();
         obj1.brand = "Samsung";
         obj1.cost = 200;
-        obj1.name = "SmartPhone";
+        Mobile.name = "SmartPhone";
 
         Mobile obj2 = new Mobile();
         obj2.brand = "Apple";
         obj2.cost = 150;
-        obj2.name = "SmartPhone";
+        Mobile.name = "SmartPhone";
+
+        Mobile.name = "FlipPhone";
 
         obj1.show();
         obj2.show();
